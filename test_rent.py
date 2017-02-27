5b7e986f9a069c261a8a4b5a99c4b8cf2f75084d
Wadood Chaudhary <wc2595@columbia.edu>
2017-02-27 13:18:59 -0500

    Home work 2
    Assignment

---
+++ test_rent.py
@@ -0,0 +1,10 @@
+import homework2_rent as p
+
+"""
+Created on Sun Feb 19 18:38:39 2017
+
+@author: Christine lee and Wadood Chaudhary
+"""
+
+if __name__ == "__main__":
+    assert(p.score_rent()>0.50)
